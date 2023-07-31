e4d8d9485d9b4380b55c9f6204c09a4a748b2eae
Kounenri <kounen.ri@gmail.com>
2023-07-31 14:56:35 +0900

    Adjust animation speed.

---
+++ Assets/Scripts/Utils/GlobalCanvas.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class GlobalCanvas : MonoBehaviour
+{
+	private static GlobalCanvas g_Instance;
+
+	public static GlobalCanvas GetInstance
+	{
+		get
+		{
+			return g_Instance;
+		}
+	}
+
+	public static bool HasInstance
+	{
+		get
+		{
+			return g_Instance != null;
+		}
+	}
+
+	[SerializeField]
+	private Camera m_TargetCamera = null;
+	private Camera m_Camera;
+	private Transform m_CameraTransform;
+
+	public Camera Camera
+	{
+		get
+		{
+			return m_Camera;
+		}
+	}
+
+	public Transform CameraTransform
+	{
+		get
+		{
+			return m_CameraTransform;
+		}
+	}
+
+	void Awake()
+	{
+		g_Instance = this;
+
+		m_Camera = GetComponentInChildren<Camera>();
+		m_CameraTransform = m_Camera.transform;
+
+		if (m_TargetCamera != null && m_TargetCamera.orthographic)
+		{
+			m_Camera.orthographic = true;
+			m_Camera.farClipPlane = 10000f;
+		}
+	}
+
+	void OnDestroy()
+	{
+		g_Instance = null;
+	}
+
+	void Update()
+	{
+		if (m_TargetCamera != null)
+		{
+			m_CameraTransform.position = m_TargetCamera.transform.position;
+			m_CameraTransform.rotation = m_TargetCamera.transform.rotation;
+			if (m_Camera.orthographic)
+			{
+				GetComponent<Camera>().orthographicSize = m_TargetCamera.orthographicSize;
+			}
+			else
+			{
+				GetComponent<Camera>().fieldOfView = m_TargetCamera.fieldOfView;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Utils/GlobalCanvasDelegate.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class GlobalCanvasDelegate : MonoBehaviour
+{
+	[SerializeField]
+	private bool m_Moveable = true;
+
+	private Transform m_ViewTransform;
+	private GameObject m_View;
+	private bool m_Invalidate = false;
+
+	public Transform ViewTransform
+	{
+		get
+		{
+			if (m_ViewTransform == null)
+			{
+				m_ViewTransform = transform.Find("Panel");
+			}
+			return m_ViewTransform;
+		}
+	}
+
+	public GameObject View
+	{
+		get
+		{
+			if (m_View == null)
+			{
+				m_View = ViewTransform.gameObject;
+			}
+			return m_View;
+		}
+	}
+
+	private void Awake()
+	{
+		m_ViewTransform = transform.Find("Panel");
+		m_View = m_ViewTransform.gameObject;
+	}
+
+	private void Start()
+	{
+		if (GlobalCanvas.HasInstance)
+		{
+			View.GetComponent<RectTransform>().SetParent(GlobalCanvas.GetInstance.transform, true);
+			m_Invalidate = true;
+		}
+	}
+
+	private void Update()
+	{
+		if (m_Moveable || m_Invalidate)
+		{
+			if (m_View != null)
+			{
+				m_View.transform.position = transform.position;
+			}
+			m_Invalidate = false;
+		}
+	}
+
+	private void OnDisable()
+	{
+		if (m_View != null)
+		{
+			m_View.SetActive(false);
+		}
+	}
+
+	private void OnEnable()
+	{
+		if (m_View != null)
+		{
+			m_Invalidate = true;
+			m_View.SetActive(true);
+
+			m_View.transform.position = transform.position;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (m_View != null) Destroy(m_View);
+
+		m_View = null;
+	}
+}
